bd12f5dd624e9406ca83f9c41bece04e6c102ca7
Nigel Banks <nigel.g.banks@gmail.com>
2014-06-05 23:01:25 -0300

    Created sub module with sample objects for testing and development, also hopefully fixed the failing travis tests (was missing install hooks). Most of the apparatus functionality has returned.

---
+++ js/apparatus.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Ties links in Apparatus block to vertical tab display.
+ */
+(function ($) {
+  "use strict";
+  Drupal.behaviors.apparatus = {
+    attach: function (context, settings) {
+      // Link each apparatus component link in the block to the proper fieldset.
+    }
+  };
+}(jQuery));
