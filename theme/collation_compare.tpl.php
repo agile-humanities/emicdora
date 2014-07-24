c9331664d68b6db8b943dd21b54024bb6d6140e3
Alan Stanley <astanley@upei.ca>
2014-07-24 14:19:00 -0300

    themed collation basic collation view

---
+++ theme/collation_compare.tpl.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all.css">
+<link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all-gray.css">
+
+<div id="metadata"></div>
+<div id="uiplaceholder"></div>
