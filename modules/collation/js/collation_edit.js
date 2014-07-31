9a012ed3d81690dd5430573ecd3862bc6f684da2
Alan Stanley <astanley@upei.ca>
2014-07-31 14:57:49 -0300

    coder

---
+++ modules/collation/js/collation_edit.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ *  Handles the versionable object ingest form specialy javascript nonsense.
+ */
+(function($) {
+  "use strict";
+
+  // function to trigger a form button when clicking on a link element.
+  Drupal.behaviors.emicdoraEdit = {
+    attach: function(context, settings) {
+
+
+    }
+  };
+})(jQuery);
--- modules/collation/theme/collation_compare.tpl.php
+++ modules/collation/theme/collation_compare.tpl.php
@@ -3,3 +3,6 @@
 
 <div id="metadata"></div>
 <div id="uiplaceholder"></div>
+<input id="emicdora_1"  type="text" style="width: 30%; -moz-user-select: text;" readonly="readonly">
+<input id="emicdora_2"  type="text" style="width: 30%; -moz-user-select: text;" readonly="readonly">
+<input id="edit-submit" class="form-submit" type="submit" value="Save changes">
