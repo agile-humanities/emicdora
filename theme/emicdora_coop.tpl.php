3092ec8b54f9890f69acaf21f120871d410688eb
Nigel Banks <nigel.g.banks@gmail.com>
2014-08-29 13:37:25 -0400

    Made recommended changes.

--- theme/emicdora_coop.tpl.php
+++ theme/emicdora_coop.tpl.php
@@ -5,22 +5,19 @@
  */
 ?>
 <div class="emicdora-coop">
-  <div style="float:right;">
-    <div class="add-to-coop" style="float:left;">
-      <label for="add-to-coop">Add to CO-OP</label>
+  <div class="emicdora-add-to-coop">
+    <div class="emicdora-add-to-coop-select">
+      <label for="add-to-coop"><?php print t('Add to CO-OP'); ?></label>
       <select id="add-to-coop" onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
-        <option value="none">Select...</option>
+        <option value="none"><?php print t('Select...'); ?></option>
         <?php foreach ($add_source_links as $item): ?>
           <option value="<?php print $item['href']; ?>"><?php print $item['title']; ?></option>
         <?php endforeach; ?>
       </select>
     </div>
-    <?php print theme_image(array(
-      'path' => drupal_get_path('module', 'emicdora') . '/images/coop.png',
-      'attributes' => array('style' => 'float:right;'),
-    )); ?>
+    <?php print $coop_icon?>
   </div>
-  <p>Welcome to the COOP. This is where all the source material for the Critical </br> Editions resides. We have grouped the content by various categories.</p>
-  <a href='islandora/search/NOT RELS_EXT_hasModel_uri_mt:(criticalEditionContainerCModel OR versionCModel)'>Browse All</a>
+  <p><?php print t('Welcome to the COOP. This is where all the source material for the Critical </br> Editions resides. We have grouped the content by various categories.'); ?></p>
+  <?php print $browse_all_link; ?>
   <div class="clearfix"></div>
 </div>
