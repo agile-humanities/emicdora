ba7ee4bd64ccf7728c5a9eb1bd728367ba924568
Alan Stanley <astanley@upei.ca>
2014-09-03 13:43:11 -0300

    sanitized callbacks

--- modules/collation/theme/collation_compare.tpl.php
+++ modules/collation/theme/collation_compare.tpl.php
@@ -1,19 +1,16 @@
 <link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all.css">
 <link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all-gray.css">
-
-<div id="metadata"></div>
-<div id="uiplaceholder"></div>
-<input id="diff_l"  type="text" style="width: 40%; -moz-user-select: text;" readonly="readonly" class="emicdora_input">
-<input id="diff_r"  type="text" style="width: 40%; -moz-user-select: text;" readonly="readonly" class="emicdora_input">
-<button id="collation_link">Merge Text</button>
-<br />
-<input id="merged_text"  type="text" style="width: 80%; -moz-user-select: text;" readonly="readonly" class="emicdora_input">
-<button id="collation_unlink" class="form-submit" type="submit" value="Unlink">Unlink</button><br/>
-<button id="save_changes">Save Changes</button><br />
-<p id ="emicdora_status"></p>
-<div id="collatex_iframe">
-  <iframe src ="http://astanley-vagrant-emic.local:7369" width="100%" height ="520px"></iframe>
+<div id ="emicdora_collation_container">
+  <div id="metadata"></div>
+  <div id="uiplaceholder"></div>
+  <input id="diff_l"  type="text" style="width: 40%; -moz-user-select: text;" readonly="readonly" class="emicdora_input">
+  <input id="diff_r"  type="text" style="width: 40%; -moz-user-select: text;" readonly="readonly" class="emicdora_input">
+  <button id="collation_link">Merge Text</button>
+  <br />
+  <input id="merged_text"  type="text" style="width: 80%; -moz-user-select: text;" readonly="readonly" class="emicdora_input">
+  <button id="collation_unlink" class="form-submit" type="submit" value="Unlink">Unlink</button><br/>
+  <button id="save_changes">Save Changes</button><br />
+  <p id ="emicdora_status"></p>
 </div>
-
 
 
