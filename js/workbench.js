34ec2b44c9b023f9541a8fb2bd7c4ff0f7ecabd9
Jordan Dukart <jordan@discoverygarden.ca>
2014-09-03 14:47:45 -0300

    Hook up workbench functionality and styling.

---
+++ js/workbench.js
@@ -0,0 +1,18 @@
+(function ($) {
+    Drupal.behaviors.emicdoraWorkbenchLink = {
+        attach: function (context, settings) {
+            $('.emicdora-workbench-entity-links').change(function() {
+              var selected = $('.emicdora-workbench-entity-links option:selected').val();
+              if (selected != 'none') {
+                window.location = Drupal.settings.basePath + selected;
+              }
+            });
+            $('.emicdora-workbench-source-links').change(function() {
+                var selected = $('.emicdora-workbench-source-links option:selected').val();
+                if (selected != 'none') {
+                  window.location = Drupal.settings.basePath + 'workbench/add/' + selected;
+                }
+            });
+        }
+    };
+})(jQuery);
