ea88545b9ef11031d4a6cb4ba191004281a900bc
Alan Stanley <astanley@upei.ca>
2014-09-15 15:34:08 -0300

    made collation editing permission dependent

--- modules/collation/theme/collation_compare.tpl.php
+++ modules/collation/theme/collation_compare.tpl.php
@@ -1,8 +1,11 @@
 <link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all.css">
 <link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all-gray.css">
-<div id ="emicdora_collation_container">
-  <div id="metadata"></div>
-  <div id="uiplaceholder"></div>
+<?php
+print '<div id ="emicdora_collation_container">';
+print '<div id="metadata"></div>
+    <div id="uiplaceholder"></div>';
+if (user_access(EMICDORA_EDIT_COLLATION)) {
+  print '
   <input id="diff_l"  type="text" style="width: 40%; -moz-user-select: text;" readonly="readonly" class="emicdora_input">
   <input id="diff_r"  type="text" style="width: 40%; -moz-user-select: text;" readonly="readonly" class="emicdora_input">
   <button class = "emicdora_button" id="collation_link">Merge Text</button>
@@ -14,6 +17,7 @@
     <div id="collatex_iframe">
     <iframe id ="emicdora_collatex_iframe" src ="/collatex/" ></iframe>
   </div>
-</div>
-
-
+  ';
+}
+print '</div>';
+?>
