0d98ac77756cd42737af1837b9bd980066ef6552
Alan Stanley <astanley@upei.ca>
2014-09-22 09:18:47 -0300

    lined code up with Nigel's suggestions

--- js/workbench.js
+++ js/workbench.js
@@ -1,19 +1,19 @@
 (function ($) {
     Drupal.behaviors.emicdoraWorkbenchLink = {
         attach: function (context, settings) {
-            $('.emicdora-workbench-entity-links').change(function() {
+            $('.emicdora-workbench-entity-links').once().change(function(){
               var selected = $('.emicdora-workbench-entity-links option:selected').val();
               if (selected != 'none') {
                 window.location = Drupal.settings.basePath + selected;
               }
             });
-            $('.emicdora-workbench-source-links').change(function() {
+            $('.emicdora-workbench-source-links').once().change(function(){
                 var selected = $('.emicdora-workbench-source-links option:selected').val();
                 if (selected != 'none') {
                   window.location = Drupal.settings.basePath + 'emicdora/source/add/' + selected + '/FALSE/TRUE';
-
                 }
             });
         }
     };
 })(jQuery);
+
