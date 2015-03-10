9a7e32e6219093213a9a55f29dd5fcd968fdae53
ajstanley <alanjarlathstanley@gmail.com>
2015-03-10 11:00:54 -0300

    added logo back, added hovertext

--- modules/collation/theme/collation_compare.tpl.php
+++ modules/collation/theme/collation_compare.tpl.php
@@ -9,32 +9,34 @@
 <div id ="emicdora_collation_container">
   <div id="metadata"></div>
   <div id="uiplaceholder"></div>
-    <?php if (user_access(COLLATION_EDIT_COLLATION) && $variables['display_type'] == 'compare'): ?>
-      <h3 id="merge_label" class="ui-widget-header field_label">Associate text</h3>
-      <div id="merge_container" class="emicdora_edit_container">
-        <h3 id="top-label" class="ui-widget-header">Version 1</h3>
-        <div id="diff_l" class="ui-widget-content collation_resize emcidora_input" ></div>
-        <h3 id="bottom-label" class="ui-widget-header">Version 2</h3>
-        <div id="diff_r" class="ui-widget-content collation_resize emcidora_input" ></div>
-        <button class = "emicdora_button" id="collation_link">Merge</button>
-        <button class = "emicdora_button" id="collation_variant" class="form-submit" type="submit" value="Variant">Variant</button>
-        <br/>
-      </div>
-      <h3 id="unmerge_label" class="ui-widget-header field_label">Disassociate text</h3>
-      <div id="unmerge_container" class="emicdora_edit_container">
-        <h3 id="merged" class="ui-widget-header">Merged text</h3>
-        <div id="merged_text" class="ui-widget-content merged_resize emcidora_input"></div>
-        <button class = "emicdora_button" id="collation_unlink">Unmerge</button><br/>
-      </div>
-      <button class = "emicdora_button" id="save_changes">Save Changes</button>
-    <?php endif; ?>
-    <br/>
-    <div id="collatex_iframe">
-      <input id="full-window-button" class="emicdora_button" type="button" value="<?php print t("Full Window"); ?>" />
-        <p>To use CollateX simply copy and paste desired text from windows above into each of the Witness boxes below.<br />
+  <?php if (user_access(COLLATION_EDIT_COLLATION) && $variables['display_type'] == 'compare'): ?>
+    <h3 id="merge_label" class="ui-widget-header field_label">Associate text</h3>
+    <div id="merge_container" class="emicdora_edit_container">
+      <h3 id="top-label" class="ui-widget-header">Version 1</h3>
+      <div id="diff_l" class="ui-widget-content collation_resize emcidora_input" ></div>
+      <h3 id="bottom-label" class="ui-widget-header">Version 2</h3>
+      <div id="diff_r" class="ui-widget-content collation_resize emcidora_input" ></div>
+      <button class = "emicdora_button" id="collation_link">Merge</button>
+      <button class = "emicdora_button" id="collation_variant" class="form-submit" type="submit" value="Variant">Variant</button>
+      <br/>
+    </div>
+    <h3 id="unmerge_label" class="ui-widget-header field_label">Disassociate text</h3>
+    <div id="unmerge_container" class="emicdora_edit_container">
+      <h3 id="merged" class="ui-widget-header">Merged text</h3>
+      <div id="merged_text" class="ui-widget-content merged_resize emcidora_input"></div>
+      <button class = "emicdora_button" id="collation_unlink">Unmerge</button><br/>
+    </div>
+    <button class = "emicdora_button" id="save_changes">Save Changes</button>
+  <?php endif; ?>
+  <br/>
+  <div id="collatex_iframe">
+    <input id="full-window-button" class="emicdora_button" type="button" value="<?php print t("Full Window"); ?>" />
+    <div id ="emicdora_collatex_info_window" title = 'Copyright © 2010-2013 The Interedition Development Group. All rights reserved.'>
+      <p>To use <a href="http://collatex.net/">CollateX</a> simply copy and paste desired text from windows above into each of the Witness boxes below.<br />
         Press 'Collate' to generate results.<br />
         Press 'Add' to open a fresh witness box to collate additional material.
-        </p>
-      <iframe id ="emicdora_collatex_iframe" src ="/collatex/" ></iframe>
+      </p>
     </div>
+    <iframe id ="emicdora_collatex_iframe" src ="/collatex/" ></iframe>
+  </div>
 </div>
