30d6f3fa1ae886dffc1128be0a512b9954e4900c
Matthew Perry <mperry@discoverygarden.ca>
2015-07-15 13:04:30 -0300

    Updates.

--- modules/version_viewer/js/init.js
+++ modules/version_viewer/js/init.js
@@ -743,7 +743,7 @@
      * Add tree tooltips for entities and text image annotations.
      */
     function add_tree_tooltips_for_entities_and_text_image_annotations() {
-      var nodes = new Array()
+      var nodes = new Array();
       var entities = $("#easyui_tree").tree('find', 'tree_entities');
       if (entities) {
         nodes.push(entities);
@@ -752,22 +752,19 @@
       if (text_image_links) {
         nodes.push(text_image_links);
       }
-      if (typeof nodes != "undefined" && nodes != null && nodes.length > 0) {
-        for (var a = 0; a < nodes.length; a++) {
-          var children = nodes[a]['children'];
-          var data = "";
-          for (var i = 0; i < children.length; i++) {
-            data = children[i]['attributes']['cwrcAttributes'];
-            var tool_tip_content = null;
-            if (data['cwrcInfo'].hasOwnProperty('description')) {
-              tool_tip_content = data['cwrcInfo']['description'];
-            }
-            if (tool_tip_content) {
-              $("#" + children[i].domId).tooltip({
-                position: 'right',
-                content: '<div class="easyui-panel" style="width:250px;height:\'auto\';padding:10px;">' + tool_tip_content + '</div>'
-              }).show();
-            }
+      for (var a = 0; a < nodes.length; a++) {
+        var children = nodes[a]['children'];
+        for (var i = 0; i < children.length; i++) {
+          var data = children[i]['attributes']['cwrcAttributes'];
+          var tool_tip_content = null;
+          if (data['cwrcInfo'].hasOwnProperty('description')) {
+            tool_tip_content = data['cwrcInfo']['description'];
+          }
+          if (tool_tip_content) {
+            $("#" + children[i].domId).tooltip({
+              position: 'right',
+              content: '<div class="easyui-panel" style="width:250px;height:\'auto\';padding:10px;">' + tool_tip_content + '</div>'
+            }).show();
           }
         }
       }
